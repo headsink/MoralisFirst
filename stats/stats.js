2073fd6bddb67b33994cf0e62e3ed9c9bacaa832
headsink <headsink@gmail.com>
2022-07-11 18:58:30 +0800

    Failed at writing File from Moralis

--- stats/stats.js
+++ stats/stats.js
@@ -4,15 +4,30 @@ const fs = require("fs");
 const ServerURL = "https://ndgc4rqck3jt.usemoralis.com:2053/server";
 const AppID = "v8f8fej8R8CTF83IUjR1t8X5gjkLDvORJY0ZFzKv";
 
-const options = {};
+const options = {
+    chain: "bsc",
+    address: "0x92E53E51A088Af8D6D3e596C6C0036fb5871935c"
+};
 
-await Moralis.start({ serverUrl: ServerURL, appId: AppID });
+async function start()
+{
+    try{
+        await Moralis.start({ serverUrl: ServerURL, appId: AppID }).then(KO8NFTs());
+    } catch(error){
+        console.error(error);
+
+    }
+    
+}
 
 const KO8NFTs = async () => {await Moralis.Web3API.account.getNFTs(options).then(async (theDatas)=>{
     try{
-        fs.writeFile('data.json',JSON.stringify(theDatas.result))
+        fs.writeFile('data.json',theDatas.result);
+        console.log("done!");
     }
-    catch{}
+    catch(error){
+        console.error(error);
+    }
 });}
 
-KO8NFTs();
+start();
